35a2535e119fc5822951f4e225898c8b339b135a
Wesley Scholl <clients@konjoweb.com>
2023-02-06 21:10:23 -0500

    built full crud api

--- cmd/main.go
+++ cmd/main.go
@@ -3,17 +3,21 @@ package main
 import (
     "log"
     "net/http"
-  "encoding/json"
 
     "github.com/gorilla/mux"
+    "github.com/karanpratapsingh/tutorials/go/crud/pkg/handlers"
 )
 
 func main() {
     router := mux.NewRouter()
 
-    router.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
-        json.NewEncoder(w).Encode("Hello World")
-    })
+    // Here we'll define our api endpoints
+
+    router.HandleFunc("/books", handlers.GetAllBooks).Methods(http.MethodGet)
+    router.HandleFunc("/books", handlers.AddBook).Methods(http.MethodPost)
+    router.HandleFunc("/books/{id}", handlers.GetBook).Methods(http.MethodGet)
+    router.HandleFunc("/books/{id}", handlers.UpdateBook).Methods(http.MethodPut)
+    router.HandleFunc("/books/{id}", handlers.DeleteBook).Methods(http.MethodDelete)
 
     log.Println("API is running!")
     http.ListenAndServe(":4000", router)
