72fe9a3b8755c96389e80c4a62261fa657a485fc
Wesley Scholl <clients@konjoweb.com>
2023-02-06 22:28:06 -0500

    updated backend

--- pkg/mocks/book.go
+++ pkg/mocks/book.go
@@ -5,8 +5,32 @@ import "github.com/karanpratapsingh/tutorials/go/crud/pkg/models"
 var Books = []models.Book{
     {
         Id:     1,
-        Title:  "Golang",
-        Author: "Gopher",
-        Desc:   "A book for Go",
+        Title:  "Chamber of Secrets",
+        Author: "JK Rowling",
+        Desc:   "Harry Potter goes to school for the 2nd year.",
+    },
+    {
+        Id:     2,
+        Title:  "The Hobbit",
+        Author: "JRR Tolkein",
+        Desc:   "Hobbits, wizards, elves, galore.",
+    },
+    {
+        Id:     3,
+        Title:  "Enders Game",
+        Author: "Orson Scott Card",
+        Desc:   "Child sci-fi fantasy, inscets vs humans.",
+    },
+    {
+        Id:     4,
+        Title:  "Outliers",
+        Author: "Malcom Gladwell",
+        Desc:   "A book about successful people who put in 10k hours.",
+    },
+    {
+        Id:     5,
+        Title:  "Drunk Tank Pink",
+        Author: "Adam Grant",
+        Desc:   "A book about Human Psycology and why people do what they do.",
     },
 }
